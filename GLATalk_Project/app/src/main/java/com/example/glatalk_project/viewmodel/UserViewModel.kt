5171aaea2e19b427e401194146e241917d78061a
강민아 <getupmina0405@gmail.com>
2020-11-07 21:15:07 +0900

    UserViewModel에 UseRequest와 UserResponse연결

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/viewmodel/UserViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.glatalk_project.viewmodel
+
+import com.example.glatalk_project.base.BaseViewModel
+import com.example.glatalk_project.model.UserModel
+
+class UserViewModel (private val model: UserModel):BaseViewModel(){
+
+    fun add(userRequest:UserResponse){
+
+    }
+}
