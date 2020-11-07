c72a8edb80e1677f410844bea6f4577cd0959c46
강민아 <getupmina0405@gmail.com>
2020-11-07 21:15:23 +0900

    layout 연결

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/view/RegisterTouristActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/view/RegisterTouristActivity.kt
@@ -1,2 +1,14 @@
 package com.example.glatalk_project.view
 
+import com.example.glatalk_project.R
+import com.example.glatalk_project.base.BaseView
+import com.example.glatalk_project.databinding.ActivityRegistBinding
+import com.example.glatalk_project.viewmodel.UserViewModel
+
+class RegisterTouristActivity : BaseView<ActivityRegistBinding,UserViewModel>(){
+
+    override val layoutResourceId: Int
+
+        get() = R.layout.activity_regist
+    override lateinit var viewModel: UserViewModel
+}
