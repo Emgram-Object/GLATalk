7f8b645c315772cf8784a63f93f965030130a918
강민아 <getupmina0405@gmail.com>
2020-11-07 22:16:55 +0900

    composite Disposable 추가함

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/base/BaseViewModel.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/base/BaseViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.glatalk_project.base;
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
-public class BaseViewModel: ViewModel(){
+
+open class BaseViewModel: ViewModel(){
+    private val compositeDisposable = CompositeDisposable()
+
+
+    fun addDisposable(disposable: Disposable){
+        compositeDisposable.add(disposable)
+    } //얘기 뭔지 모르겠음 나중에 지멘한테 물어봐야징
+
 
 }
