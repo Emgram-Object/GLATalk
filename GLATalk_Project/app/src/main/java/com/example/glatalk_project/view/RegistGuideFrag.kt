8a7d1cb0f82ff90d9c6b28e6bb462f6e1de2fced
강민아 <getupmina0405@gmail.com>
2020-11-08 20:57:35 +0900

    Regist View를 하나로 합치고 Fragment클래스를 guide와 tourist로 나눠서 생성함

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/view/RegistGuideFrag.kt
@@ -0,0 +1,8 @@
+package com.example.glatalk_project.view
+
+import androidx.fragment.app.FragmentActivity
+import com.example.glatalk_project.base.BaseFragment
+
+class RegistGuideFrag :BaseFragment<> {
+
+}
--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/view/RegisterGuideActivity.kt
+++
@@ -1,2 +0,0 @@
-package com.example.glatalk_project.view
-
