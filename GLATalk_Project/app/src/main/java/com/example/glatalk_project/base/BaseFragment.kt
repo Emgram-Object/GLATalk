ed3c463ff9bb26d0a4a67552f4b41bd1a2332637
강민아 <getupmina0405@gmail.com>
2020-11-08 20:58:27 +0900

    Fragment 를 MVVM구조에 적용할 수 있도록 baseFragment 클래스를 만듦

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/base/BaseFragment.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/base/BaseFragment.kt
@@ -1,4 +1,15 @@
 package com.example.glatalk_project.base;
 
-public class BaseFragment{
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseFragment<T:ViewDataBinding,U:BaseViewModel>{
+    lateinit var viewDataBinding: T
+    abstract val layoutResourceId: Int
+    abstract var viewModel: U
+
+
+    abstract fun initView()
+    abstract fun initDataBinding()
+    abstract fun initListener()
+    //abstract fun invalidate()
 }
