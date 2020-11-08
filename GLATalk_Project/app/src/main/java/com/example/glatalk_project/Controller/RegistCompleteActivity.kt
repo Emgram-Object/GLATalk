3b984719b3de905885ae9df65acca7291414c8f1
kencoor <kencoor@naver.com>
2020-11-09 02:32:36 +0900

    RegistActivity 컨트롤러 구현

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Controller/RegistCompleteActivity.kt
@@ -0,0 +1,19 @@
+package com.example.glatalk_project.Controller
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.glatalk_project.R
+import kotlinx.android.synthetic.main.activity_regist_complete.*
+
+class RegistCompleteActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regist_complete)
+
+        back_login_btn.setOnClickListener {
+            val nextIntent = Intent(this, LoginActivity::class.java)
+            startActivity(nextIntent)
+        }
+    }
+}
