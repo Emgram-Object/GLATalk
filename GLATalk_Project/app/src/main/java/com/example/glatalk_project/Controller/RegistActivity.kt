bca787f0e42598d95c8d7fdb61a4a1315d58a8d8
강민아 <getupmina0405@gmail.com>
2020-11-09 01:11:31 +0900

    activity 생성 및 fragment 추가

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Controller/RegistActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Controller/RegistActivity.kt
@@ -1,8 +1,43 @@
 package com.example.glatalk_project.Controller
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.glatalk_project.R
 
 
-class RegistActivity:Fragment() {
+class RegistActivity:AppCompatActivity() {
 
+    lateinit var guideFragment: registGuideFragment
+    lateinit var touristFragment: registTouristFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regist)
+        R.id.reg_tour_radio_bnt
+    }
+    inner class changeGuide:View.OnClickListener{
+        override fun onClick(v: View?) {
+            when(v?.id){
+                R.id.reg_tour_radio_bnt -> changeTouristFragment()
+                R.id.reg_guide_radio_bnt -> changeGuideFragement()
+            }
+        }
+    }
+
+    fun changeTouristFragment(){
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.reg_info_fl,touristFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+
+    }
+    fun changeGuideFragement(){
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.reg_info_fl,guideFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+    }
 }
---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Controller/registTouristFragment.kt
@@ -0,0 +1,6 @@
+package com.example.glatalk_project.Controller
+
+import androidx.fragment.app.Fragment
+
+class registTouristFragment:Fragment() {
+}
