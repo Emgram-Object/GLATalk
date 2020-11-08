822fa4d233b53532e68dcc5ec27bc22804e10830
하지현 <>
2020-11-09 08:42:35 +0900

    정보 수정 액티비티

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Controller/TouristInfoChangeActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Controller/TouristInfoChangeActivity.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.glatalk_project.R
 import android.widget.EditText
-
+import android.widget.Button
 
 class TouristInfoChangeActivity:AppCompatActivity(){
 
@@ -13,19 +13,30 @@ class TouristInfoChangeActivity:AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_info)
 
-        //기존 정보 받아와서 표시하기 //서버에서 받아오기
+        //기존 정보 서버에서 받아와서 표시
 
-        val Name = findViewById(R.id.my_info_name_et) as EditText
-        val Phone = findViewById<EditText>(R.id.my_info_phone_et)
-        val Email = findViewById<EditText>(R.id.my_info_email_et)
+        val user_name = findViewById<EditText>(R.id.my_info_name_et).text.toString()
+        val mobile_num = findViewById<EditText>(R.id.my_info_phone_et).text.toString()
+        val user_email = findViewById<EditText>(R.id.my_info_email_et).text.toString()
 
        // user_name.setText("")
-
-        val user_name =  Name.text
-        val mobile_num = Phone.text
+        /*
+        val user_name = my_info_name_et.text.toString()
+        val mobile_num = Phone.text.toString()
         val user_email = Email.text
+        */
+        val btn_Scene = findViewById<Button>(R.id.my_info_pwd_change_btn)
 
-
-
+        //패스워드 바꾸는 Activity로 넘어가기
+        btn_Scene.setOnClickListener{
+            //startActivity(Intent(this, PwdChangeActivity::class.java))
+            //비밀번호 변경 액티비티 만들면 넣어주기
+            finish()
+        }
     }
+
+
+
+    //후에 버튼 누르면 서버로 넘겨주기
+
 }
