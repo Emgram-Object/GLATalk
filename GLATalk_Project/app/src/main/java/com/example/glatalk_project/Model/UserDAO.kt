77d4f5b89bb1a2edef228d952c520de81972e28a
강민아 <getupmina0405@gmail.com>
2020-11-09 21:15:08 +0900

    object Context 클래스 생성

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/UserDAO.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/UserDAO.kt
@@ -1,5 +1,34 @@
 package com.example.glatalk_project.Model
 
-class UserDAO {
+import android.util.Log
+import com.example.glatalk_project.constant.C
+import com.example.glatalk_project.util.PreferenceUtil
 
+class UserDAO {
+    var accessToken: String? =""
+    var user_id:String?=""
+    var country_cd:String =""
+    var language_code:String =""
+    set(lang) {
+        PreferenceUtil.putStringSync(C.Preference.SELECTED_LANGUAGE,lang)
+        field = lang
+        Log.d("UserDAO","lang: "+lang)
+    }
+    init {
+        load()
+    }
+    private fun load(){
+        language_code = PreferenceUtil.getString(C.Preference.SELECTED_LANGUAGE)
+    }
+
+    companion object{
+        private var _instance:UserDAO?=null
+        val instance:UserDAO
+        get() {
+            if (_instance==null){
+                _instance = UserDAO()
+            }
+            return _instance!!
+        }
+    }
 }
---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/constant/C.kt
@@ -0,0 +1,15 @@
+package com.example.glatalk_project.constant
+
+object C {
+    object Preference{
+        const val KEY_ACCESS_TOKEN = "KEY_ACCESS_TOKEN"
+        const val KEY_IS_AUTO_LOGIN = "KEY_IS_AUTO_LOGIN"
+        const val KEY_USER_ID = "KEY_USER_ID"
+        //const val PUSH_SETTING_CHECKED = "PUSH_SETTING_CHECKED"
+        const val PERMISSION_SETTING_CHECKED = "PERMISSION_SETTING_CHECKED"
+        const val SELECTED_LANGUAGE = "Locale.Helper.Selected.Language"
+        const val SEARCH_PREF = "SEARCH_PREF"
+        const val USER_NO = "USER_NO"
+        const val FIRST_JOURNEY_CREATED = "FIRST_JOURNEY_CREATED"
+    }
+}
