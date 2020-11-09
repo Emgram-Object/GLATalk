21c7916ae6ae68da993db373490e4553830a928e
강민아 <getupmina0405@gmail.com>
2020-11-09 21:16:39 +0900

    TM클래스 생성

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/network/TMApiInterceptor.kt
@@ -0,0 +1,29 @@
+package com.example.glatalk_project.network
+
+import android.util.Log
+import com.example.glatalk_project.Model.UserDAO
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+class TMApiInterceptor:Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+//        MainApplication.application?.let {
+//            if(!request.url().host().contains(it.getString(R.string.base_url))) {
+//                return chain.proceed(request)
+//            } else {
+        val token = UserDAO.instance.accessToken
+        Log.d("App Token", token ?: "")
+        if (token != null && token.isNotEmpty()) {
+            builder.addHeader("Authorization", token)
+        }
+        builder.addHeader("Accept-Language", UserDAO.instance.language_code)
+
+        //}
+//        }
+
+        return chain.proceed(builder.build())
+    }
+}
