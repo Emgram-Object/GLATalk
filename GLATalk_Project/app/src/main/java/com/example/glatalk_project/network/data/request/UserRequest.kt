c87f933dbdee9f37be2b2c90714949bab1ceb5ff
강민아 <getupmina0405@gmail.com>
2020-11-09 21:17:34 +0900

    data 클래스 생성

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/network/data/request/UserRequest.kt
@@ -0,0 +1,13 @@
+package com.example.glatalk_project.network.data.request
+
+data class UserRequest (
+        val user_name:String,
+        val user_email:String,
+        val user_type:String,
+        val user_pwd:String,
+        val mobile_num:String,
+        val country_cd:String,
+        val guide_info:String,
+        val guide_time:String,
+        val ad_agree:Boolean
+)
