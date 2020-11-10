97dc36308e320513ed29d747f413197ead288bb9
강민아 <getupmina0405@gmail.com>
2020-11-10 11:46:48 +0900

    Protocol 수정, UserDAO 수정

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/UserDAO.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/UserDAO.kt
@@ -2,33 +2,56 @@ package com.example.glatalk_project.Model
 
 import android.util.Log
 import com.example.glatalk_project.constant.C
+import com.example.glatalk_project.network.BaseResponse
+import com.example.glatalk_project.network.MainApiServer
+import com.example.glatalk_project.network.data.request.LangRequest
+import com.example.glatalk_project.network.data.request.LoginRequest
+import com.example.glatalk_project.network.data.request.PwdFindRequest
+import com.example.glatalk_project.network.data.request.UserRequest
+import com.example.glatalk_project.network.data.response.LoginResponse
+import com.example.glatalk_project.network.data.response.ProfileResponse
+import com.example.glatalk_project.network.error.AppError
 import com.example.glatalk_project.util.PreferenceUtil
+import io.reactivex.Single
+import retrofit2.Call
 
-class UserDAO {
-    var accessToken: String? =""
-    var user_id:String?=""
-    var country_cd:String =""
-    var language_code:String =""
-    set(lang) {
-        PreferenceUtil.putStringSync(C.Preference.SELECTED_LANGUAGE,lang)
-        field = lang
-        Log.d("UserDAO","lang: "+lang)
-    }
-    init {
-        load()
-    }
-    private fun load(){
-        language_code = PreferenceUtil.getString(C.Preference.SELECTED_LANGUAGE)
-    }
+interface UserIntDAO{
+    fun login(loginRequest: LoginRequest):Single<LoginResponse>
+    fun findPwd(findRequest: PwdFindRequest):Single<BaseResponse>
+    fun add(userRequest: UserRequest):Single<BaseResponse>
+    fun userLanguage(request: LangRequest):Single<BaseResponse>
+    fun userDetail():Single<ProfileResponse>
+}
 
-    companion object{
-        private var _instance:UserDAO?=null
-        val instance:UserDAO
-        get() {
-            if (_instance==null){
-                _instance = UserDAO()
-            }
-            return _instance!!
-        }
+
+class UserDAO:UserIntDAO {
+    override fun login(loginRequest: LoginRequest): Single<LoginResponse> {
+        return MainApiServer.API.login(loginRequest).
     }
 }
+//    var accessToken: String? =""
+//    var user_id:String?=""
+//    var country_cd:String =""
+//    var language_code:String =""
+//    set(lang) {
+//        PreferenceUtil.putStringSync(C.Preference.SELECTED_LANGUAGE,lang)
+//        field = lang
+//        Log.d("UserDAO","lang: "+lang)
+//    }
+//    init {
+//        load()
+//    }
+//    private fun load(){
+//        language_code = PreferenceUtil.getString(C.Preference.SELECTED_LANGUAGE)
+//    }
+//
+//    companion object{
+//        private var _instance:UserDAO?=null
+//        val instance:UserDAO
+//        get() {
+//            if (_instance==null){
+//                _instance = UserDAO()
+//            }
+//            return _instance!!
+//        }
+//    }
--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/network/TMApiProtocol.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/network/TMApiProtocol.kt
@@ -1,9 +1,13 @@
 package com.example.glatalk_project.network
 
 import com.example.glatalk_project.network.data.request.LangRequest
+import com.example.glatalk_project.network.data.request.LoginRequest
+import com.example.glatalk_project.network.data.response.LoginResponse
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface TMApiProtocol {
@@ -12,53 +16,81 @@ interface TMApiProtocol {
      */
     @PUT("api/v1/common/user_language")
     fun user_language(@Body langRequest: LangRequest):Call<BaseResponse>
+
+
     /**
      * 로그인
      */
+    @POST("api/v1/user/login")
+    fun login(@Body loginRequest: LoginRequest):Call<LoginResponse>
+
 
     /**
-     * 로그아웃
+     * 로그아웃 ..로그아웃은 프로토콜이 없나..?
      */
+    @GET("/v1/user/logout")
+    fun logout(@Body  )
+
 
     /**
      * 회원가입
      */
 
+
+
     /**
      * 비밀번호찾기
      */
+
+
 
     /**
      * 내정보 조회
      */
 
+
+
     /**
      * 내정보 변경
      */
+
+
 
     /**
      * 비밀번호 변경
      */
 
+
+
     /**
      * 프로필 이미지 삭제
      */
+
+
 
     /**
      * 관광객 홈
      */
 
+
+
     /**
      * 가이드 홈
      */
+
+
 
     /**
      * 대화 내역 목록
      */
 
+
+
     /**
      * 대화방 삭제
      */
+
+
 
     /**
      * 번역
