af89a5a710aadfbe45a81d101f27fc1add525748
강민아 <getupmina0405@gmail.com>
2020-11-12 03:38:15 +0900

    Spinner 보류

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/RegistActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/RegistActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
-import android.widget.EditText
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.example.glatalk_project.Model.UserDAO
-import com.example.glatalk_project.Model.UserVO
 import com.example.glatalk_project.R
+import com.example.glatalk_project.R.array.country_cd_list
 import com.example.glatalk_project.network.BaseResponse
-import com.example.glatalk_project.network.data.request.LoginRequest
 import com.example.glatalk_project.network.data.request.UserRequest
-import com.example.glatalk_project.network.data.response.LoginResponse
 import kotlinx.android.synthetic.main.activity_regist.*
 import kotlinx.android.synthetic.main.fragment_reg_guide.*
 import kotlinx.android.synthetic.main.fragment_reg_tour.*
@@ -28,7 +28,9 @@ class RegistActivity : AppCompatActivity() {
     lateinit var touristFragment: registTouristFragment
     private var userDAO = UserDAO
 
-
+//    lateinit var country_guide_sp :Spinner
+//    val country_cd = resources.getStringArray(R.array.country_cd_list)
+//    val sp_adapter = ArrayAdapter.createFromResource(this, country_cd_list, android.R.layout.simple_dropdown_item_1line)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +43,7 @@ class RegistActivity : AppCompatActivity() {
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
+
         reg_tour_radio_bnt.setOnClickListener(View.OnClickListener {
             reg_guide_radio_bnt.isChecked = false
             changeTouristFragment()
@@ -50,20 +53,37 @@ class RegistActivity : AppCompatActivity() {
             changeGuideFragement()
         })
 
-
         reg_next_btn.setOnClickListener(View.OnClickListener {
             if (reg_guide_radio_bnt.isChecked) {
                 getGuideData()
                 Log.d("log", userDAO.userVO.toString())
                 addNetworking()
 
-            }else if(reg_tour_radio_bnt.isChecked){
+            } else if (reg_tour_radio_bnt.isChecked) {
                 getTouristData()
                 Log.d("log", userDAO.userVO.toString())
                 addNetworking()
             }
         })
+
     }
+
+//    fun country_guide(){
+//        country_guide_sp.adapter = sp_adapter
+//        country_guide_sp.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                Log.d("spinner", "onNothingSelected: ")
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                when(position){
+//                    0 -> "힌국"
+//                }
+//            }
+//
+//        }
+//
+//    }
 
     fun changeTouristFragment() {
         supportFragmentManager.beginTransaction()
@@ -80,33 +100,8 @@ class RegistActivity : AppCompatActivity() {
                 .commit()
     }
 
-    fun getGuideData() {
-        userDAO.userVO.user_email = reg_email_et.text.toString()
-        userDAO.userVO.user_pwd = reg_pwd_et.text.toString()
-        userDAO.userVO.pwd_check = reg_pwd_check_et.text.toString()
-        userDAO.userVO.user_type = "guide"
-        userDAO.userVO.user_name = reg_name_et.text.toString()
-        // userDAO.userVO.country_cd = reg_country_select_tv.text.toString()  -> 스피너 값 받는거 함수만들어서 따로 처리 해서 추가.
-        userDAO.userVO.country_cd ="ko"
-        userDAO.userVO.phone_number = reg_phone_et.text.toString()
-        userDAO.userVO.guide_info = reg_guide_info_et.text.toString()
-        userDAO.userVO.guide_time = reg_time_et.text.toString()
-        userDAO.userVO.ad_agree =true
-    }
-    fun getTouristData(){
-        userDAO.userVO.user_email = reg_email_et.text.toString()
-        userDAO.userVO.user_pwd = reg_pwd_et.text.toString()
-        userDAO.userVO.pwd_check = reg_pwd_check_et.text.toString()
-        userDAO.userVO.user_type = "tourist"
-        userDAO.userVO.user_name = reg_tourist_name_et.text.toString()
-        userDAO.userVO.country_cd ="ko"
-        // userDAO.userVO.country_cd = reg_tourist_country_select_tv.text.toString()  -> 스피너 값 받는거 함수만들어서 따로 처리 해서 추가.
-        userDAO.userVO.phone_number = reg_tourist_phone_et.text.toString()
-        userDAO.userVO.ad_agree = true
-
-    }
-    fun addNetworking(){
-        UserDAO.add( userRequest = UserRequest("","","","","","","","",true)
+    fun addNetworking() {
+        UserDAO.add(userRequest = UserRequest("", "", "", "", "", "", "", "", true)
                 , callback = object : Callback<BaseResponse> {
             override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
                 Log.d("add", "fail")
@@ -120,4 +115,33 @@ class RegistActivity : AppCompatActivity() {
 
         })
     }
+
+    fun getGuideData() {
+        userDAO.userVO.user_email = reg_email_et.text.toString()
+        userDAO.userVO.user_pwd = reg_pwd_et.text.toString()
+        userDAO.userVO.pwd_check = reg_pwd_check_et.text.toString()
+        userDAO.userVO.user_type = "guide"
+        userDAO.userVO.user_name = reg_name_et.text.toString()
+        // userDAO.userVO.country_cd = reg_country_select_tv.text.toString()  -> 스피너 값 받는거 함수만들어서 따로 처리 해서 추가.
+        userDAO.userVO.country_cd = "ko"
+        userDAO.userVO.phone_number = reg_phone_et.text.toString()
+        userDAO.userVO.guide_info = reg_guide_info_et.text.toString()
+        userDAO.userVO.guide_time = reg_time_et.text.toString()
+        userDAO.userVO.ad_agree = true
+    }
+
+    fun getTouristData() {
+        userDAO.userVO.user_email = reg_email_et.text.toString()
+        userDAO.userVO.user_pwd = reg_pwd_et.text.toString()
+        userDAO.userVO.pwd_check = reg_pwd_check_et.text.toString()
+        userDAO.userVO.user_type = "tourist"
+        userDAO.userVO.user_name = reg_tourist_name_et.text.toString()
+        userDAO.userVO.country_cd = "ko"
+        // userDAO.userVO.country_cd = reg_tourist_country_select_tv.text.toString()  -> 스피너 값 받는거 함수만들어서 따로 처리 해서 추가.
+        userDAO.userVO.phone_number = reg_tourist_phone_et.text.toString()
+        userDAO.userVO.ad_agree = true
+
+    }
+
+
 }
--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/registGuideFragment.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/registGuideFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.glatalk_project.R
 
