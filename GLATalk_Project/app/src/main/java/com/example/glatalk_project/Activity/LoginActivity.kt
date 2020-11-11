4ffac0b4c9fc7ca08646585f195b83de12f95faf
강민아 <getupmina0405@gmail.com>
2020-11-11 23:49:38 +0900

    디렉토리 위치 변경

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/LoginActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.glatalk_project
+package com.example.glatalk_project.Activity
 
 import android.os.Bundle
 import android.util.Log
--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MainActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.glatalk_project;
+package com.example.glatalk_project.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
