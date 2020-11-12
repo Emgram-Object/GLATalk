12cc2d384da29495850d970bf6a1284ef8259292
kencoor <kencoor@naver.com>
2020-11-12 23:41:00 +0900

    room_custom UI수정및 이름변경 + 변경사항 각 클래스마다 적용

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/core/util/RoomVIewHolder.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/core/util/RoomVIewHolder.kt
@@ -3,14 +3,14 @@ package com.example.glatalk_project.core.util
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.glatalk_project.core.data.ChatRoom
-import kotlinx.android.synthetic.main.ui_guide_room_custom.view.*
+import kotlinx.android.synthetic.main.ui_room_custom.view.*
 
 class RoomVIewHolder(v: View): RecyclerView.ViewHolder(v) {
     var view = v
 
     fun bind(item: ChatRoom){
-        view.tour_lang_tv.text = item.language
-        view.last_time_tv.text = item.time
-        view.tour_name_tv.text = item.name
+        view.room_lang_tv.text = item.language
+        view.room_time_tv.text = item.time
+        view.room_name_tv.text = item.name
     }
 }
