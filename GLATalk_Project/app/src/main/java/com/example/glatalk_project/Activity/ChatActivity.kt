749f8cd2db5749d35c7f0402ca04798a2013c860
하지현 <>
2020-11-15 04:35:53 +0900

    no message

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/ChatActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/ChatActivity.kt
@@ -1,26 +1,67 @@
 package com.example.glatalk_project.Activity
 
+import android.service.autofill.UserData
 import androidx.appcompat.app.AppCompatActivity
 import com.example.glatalk_project.Model.ChatData
 import com.example.glatalk_project.Model.ChatModel
+import com.example.glatalk_project.Model.UserDAO
+import com.example.glatalk_project.Model.UserVO
 import com.example.glatalk_project.R
+import com.example.glatalk_project.core.adapter.ChatAdapter
+import kotlinx.android.synthetic.main.activity_chat.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChatActivity:AppCompatActivity(){
      val layoutResourceId:Int
         get()= R.layout.activity_chat
      //var viewModel: ChatViewModel
+     private lateinit var chatAdapter: ChatAdapter
+    lateinit var view: ChatView
+
+    private var UserVO = UserVO()
 
     private var roomname=""
-    private var receiver = ""
+    private var receiver_id = ""
     private var target = ""
-   // private var Guide: GuideData? = null
+    private var user_type=""
+   // private var : GuideData? = null
 
     private var isConnected = false
     private var chatData: ChatData? = null
 
     fun initView(){
         roomname = intent.getStringExtra("reserve_id")?:""
-        receiver = intent.getStringExtra("receiver_id")?:""
+        receiver_id = intent.getStringExtra("receiver_id")?:""
+        user_type = toStringGson(intent.getStringExtra("user_type")) //?????모르갯음
+
+        if(roomname.isEmpty()||receiver_id.isEmpty()){
+            finish()
+        }
+
+        chatAdapter = ChatAdapter()
+        chat_rv.adapter = chatAdapter
+
+        //viewModel.chatList(roomname)
+
+        fun initListener(){
+            chat_send_iv.setOnClickListener{
+                val df = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                val chatData = ChatData()
+                chatData.source_lang = UserVO.country_cd //userinfomanager = 유저 정보 담고 있는 거
+                chatData.target_lang = ""//받아오기
+                chatData.source_text = chat_input_et.text.toString()
+                chatData.sender_id = UserVO.user_email
+                chatData.receiver_id = receiver_id
+                chatData.sender_user_type = ""
+                chatData.receiver_user_type = ""
+                chatData.room_id = roomname
+                chatData.msg_dt = df.format(Date(System.currentTimeMillis()))
+                this.chatData = chatData
+
+            }
+        }
+
 
     }
 }
--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/ChatModel.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/ChatModel.kt
@@ -1,5 +1,6 @@
 package com.example.glatalk_project.Model
 
+import com.example.glatalk_project.network.ApiServer
 import com.example.glatalk_project.network.data.response.ChatResponse
 import com.example.glatalk_project.network.data.response.PapagoResponse
 import io.reactivex.Single
@@ -9,23 +10,24 @@ class ChatModel {
         return ApiServer.API.translation(chatData).toStirng()
     }
     fun chat_list(room_id: String): Single<ChatResponse> {
-        return MainApiServer.API.chat_list(room_id).toSingle()
+        return ApiServer.API.chat_list(room_id).toSingle()
     }
 
 }
 
 class ChatData {
-    var source: String = ""         //요청 언어
+    var source_lang: String = ""         //요청 언어
     var source_text: String = ""    //요청 텍스트
-    var target: String? = null         //번역 언어
+    var target_lang: String? = null         //번역 언어
     var target_text: String = ""    //번역 텍스트
     //var text: String = ""          //요청 메세지(번역 api 파라미터)
-    var sender: String? = ""        //보내는 사람 id
-    var receiver: String? = ""      //받는 사람 id
-    var sender_type: String? = ""   //사용자/기사 여부 -  NM
-    var receiver_type: String? = "" //사용자/기사 여부 -  DR
+    var sender_id: String? = ""        //보내는 사람 id
+    var receiver_id: String? = ""      //받는 사람 id
+    var sender_user_type: String? = ""   //관광객or가이드
+    var receiver_user_type: String? = "" //관광객or가이드
     var msg_dt: String = ""            //메세지 날짜
     var room_id: String = ""           //방 id
+    var room_member_cnt: Int?=null //인원수
 }
 
 class Chat {
@@ -34,13 +36,13 @@ class Chat {
 }
 
 data class InitData(
-        val sender: String,
-        val receiver: String,
+        val sender_id: String,
+        val receiver_id: String,
         val room_id: String
 )
 
 data class TransData(
-        val source: String,
-        val target: String,
+        val source_lang: String,
+        val target_lang: String,
         val target_text: String
 )
