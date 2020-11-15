7f92abe9aacd913aac272c6983569ee3e772e9e3
강민아 <getupmina0405@gmail.com>
2020-11-15 22:48:16 +0900

    findPwdActivity 구현(후에 팝업만 추가하면 됨)

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/PwdFindActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/PwdFindActivity.kt
@@ -1,12 +1,74 @@
 package com.example.glatalk_project.Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.glatalk_project.Model.UserDAO
 import com.example.glatalk_project.R
+import com.example.glatalk_project.UserData
+import com.example.glatalk_project.network.BaseResponse
+import com.example.glatalk_project.network.data.request.PwdFindRequest
+import kotlinx.android.synthetic.main.activity_find_pwd.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PwdFindActivity : AppCompatActivity() {
+
+    private var userDAO = UserDAO
+    var input_user_email:String= " "
+    var userData = UserData()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_pwd)
+
+        find_pwd_back_btn.setOnClickListener {
+            finish_find_pwd()
+        }
+
+        find_pwd_send.setOnClickListener {
+            input_user_email = find_pwd_id_et.text.toString()
+            send_find_pwd()
+        }
     }
+
+
+    fun send_find_pwd(){
+
+        userDAO.findPwd(pwdFindRequest = PwdFindRequest(input_user_email), callback = object : Callback<BaseResponse> {
+            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+                var result = response.body()!!
+                userData.resultCode = result.resultCode.toString()
+                userData.desc = result.desc.toString()
+
+                if (response.isSuccessful) {
+                    if (userData.resultCode == "0") {
+                        Log.d("result", "${userData.resultCode}")
+                        Log.d("result", "${userData.desc}")
+                        //발송완료 팝업창
+                    } else {
+                        //등록뙨 이메일이 아닙니다 팝업창
+                        Log.d("result", "${userData.resultCode}")
+                        Log.d("result", "${userData.desc}")
+                    }
+                }
+            }
+        })
+    }
+
+
+    fun finish_find_pwd(){
+        val intentAct = Intent(this, LoginActivity::class.java)
+        startActivity(intentAct)
+        finish()
+    }
+
+
+
 }
