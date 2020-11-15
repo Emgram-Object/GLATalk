351d94b191a6d6387af6a8fee42887e04d899430
강민아 <getupmina0405@gmail.com>
2020-11-16 05:21:10 +0900

    MyDao생성

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Model/MyDao.kt
@@ -0,0 +1,20 @@
+package com.example.glatalk_project.Model
+
+import com.example.glatalk_project.TokenData
+import com.example.glatalk_project.network.ApiServer
+import com.example.glatalk_project.network.BaseResponse
+import com.example.glatalk_project.network.data.request.PwdRequest
+import com.example.glatalk_project.network.networkInterface
+import retrofit2.Callback
+import retrofit2.http.Header
+
+object MyDao{
+    var apiServer : networkInterface
+
+    init {
+        apiServer = ApiServer.retrofit.create(networkInterface::class.java)
+    }
+    fun change_pwd( pwdRequest: PwdRequest, callback: Callback<BaseResponse>){
+        apiServer.change_pwd(TokenData.loginToken,pwdRequest).enqueue(callback)
+    }
+}
