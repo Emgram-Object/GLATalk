a025e9a5739effb2bcc7f4de8ec473c0b7cab70e
강민아 <getupmina0405@gmail.com>
2020-11-15 19:27:42 +0900

    돌아가는 버튼 생성

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/PwdFindActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/PwdFindActivity.kt
@@ -1,6 +1,33 @@
 package com.example.glatalk_project.Activity
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.glatalk_project.R
+import kotlinx.android.synthetic.main.activity_find_pwd.*
 
 class PwdFindActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_pwd)
+
+        find_pwd_send.setOnClickListener {
+
+        }
+
+        find_pwd_back_btn.setOnClickListener {
+
+        }
+    }
+    fun goBack(){
+        val intentAct = Intent(this, LoginActivity::class.java)
+        startActivity(intentAct)
+        finish()
+    }
+
+
+
+
+
 }
