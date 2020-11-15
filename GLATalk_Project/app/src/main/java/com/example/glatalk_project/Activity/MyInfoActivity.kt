ea85917edba5728d86281b2f2a4534c0a935175f
하지현 <>
2020-11-16 05:24:16 +0900

    no message

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
@@ -1,32 +1,53 @@
-package com.example.glatalk_project.Controller
+package com.example.glatalk_project.Activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.glatalk_project.Model.UserDAO
 import com.example.glatalk_project.R
-import android.widget.EditText
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_my_info.*
+import com.example.glatalk_project.TokenData
+import com.example.glatalk_project.network.BaseResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MyInfoActivity:AppCompatActivity() {
+class MyInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_info)
+        var userDao = UserDAO
 
         //common title 수정버튼 누르면 넘어감
 
 
         //기존 서버에서 받아와서 editText에 표시
 
-       //비밀번호 변경
-        my_info_pwd_change_btn.setOnClickListener{
-            Pwd_Change()
-        }
+        //비밀번호 변경
+//        my_info_pwd_change_btn.setOnClickListener{
+//            Pwd_Change()
+//        }
+        Log.d("result", "${TokenData.loginToken}")
+
+        userDao.userDetail(callback = object : Callback<BaseResponse> {
+            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+            }
+
+            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+                if (response.isSuccessful) {
+                    var result = response.body()!!.toString()
+                    Log.d("result", "$result")
+                }
+
+            }
+        })
+
+        Log.d("result", "ddddddd")
+
     }
 
-    private fun Pwd_Change(){
-        val intent = Intent(this, PwdChangeActivity::class.java)
-        startActivity(intent)
-    }
+
+//    private fun Pwd_Change(){
+//        val intent = Intent(this, PwdChangeActivity::class.java)
+//        startActivity(intent)
+//    }
 }
