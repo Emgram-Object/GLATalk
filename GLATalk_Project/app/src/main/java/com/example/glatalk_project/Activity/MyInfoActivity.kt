0efcb78a826dcb49cdfaf254b21a79610100d7a6
강민아 <getupmina0405@gmail.com>
2020-11-17 02:21:50 +0900

    코드작성

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
@@ -18,6 +18,64 @@ import retrofit2.Response
 
 class MyInfoActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info)
+        var userDao = UserDAO
 
+        //var userVo = UserVO()
+
+        //비밀번호 변경
+        my_info_pwd_change_btn.setOnClickListener {
+            Pwd_Change()
+        }
+
+
+        Log.d("result", "${TokenData.loginToken}")
+
+        userDao.userDetail(callback = object : Callback<BaseResponse> {
+            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+            }
+
+            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+                if (response.isSuccessful) {
+                    var result = response.body()!!.toString()
+                    Log.d("result", "$result")
+                }
+
+            }
+        })
+        Log.d("result", "ddddddd")
+
+        change_tv.visibility = View.VISIBLE
+
+        change_tv.setOnClickListener() {
+            InfoChange()
+        }
+
+    }
+
+
+    private fun Pwd_Change() {
+        val intent = Intent(this, PwdChangeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun InfoChange() {
+        val intent = Intent(this, InfoChangeActivity::class.java)
+        startActivity(intent)
+    }
+
+//
+//        my_info_name_et.setText(userVo.user_name)
+//        my_info_phone_et.setText(userVo.phone_number)
+//        my_info_email_et.setText(userVo.user_email)
+//        my_info_country_select_tv.setText(userVo.country_cd)
+
+
+    private fun ChangeInfo() {
+        val intent = Intent(this, InfoChangeActivity::class.java)
+        startActivity(intent)
+    }
 
 }
