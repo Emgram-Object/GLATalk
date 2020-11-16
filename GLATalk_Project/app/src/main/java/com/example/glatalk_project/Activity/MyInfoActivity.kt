761b46961ef7c7bd182d3446b9a46d3264dacfee
하지현 <>
2020-11-16 23:47:20 +0900

    common title 버튼 관련 수정

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
@@ -1,14 +1,18 @@
 package com.example.glatalk_project.Activity
 
 import android.content.Intent
+import android.icu.text.IDNA
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.galtalk_project.Controller.InfoChangeActivity
 import com.example.glatalk_project.Model.UserDAO
 import com.example.glatalk_project.R
 import com.example.glatalk_project.TokenData
 import com.example.glatalk_project.network.BaseResponse
 import kotlinx.android.synthetic.main.activity_my_info.*
+import kotlinx.android.synthetic.main.ui_common_title.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -46,11 +50,22 @@ class MyInfoActivity : AppCompatActivity() {
         })
         Log.d("result", "ddddddd")
 
+        change_tv.visibility = View.VISIBLE
+
+        change_tv.setOnClickListener(){
+            InfoChange()
+        }
+
     }
 
 
     private fun Pwd_Change(){
         val intent = Intent(this, PwdChangeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun InfoChange(){
+        val intent = Intent(this, InfoChangeActivity::class.java)
         startActivity(intent)
     }
 }
