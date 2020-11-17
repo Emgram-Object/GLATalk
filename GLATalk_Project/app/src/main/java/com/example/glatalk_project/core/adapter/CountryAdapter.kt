cabb47bdc7e646d54dbc2653da0d9a8ecd95e789
하지현 <kkim.haji@gmail.com>
2020-11-18 03:27:12 +0900

    Spinner 관련 adapter생성 & 내부 함수, xml 수정 ing

---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/core/adapter/CountryAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.glatalk_project.core.adapter
+
+import com.example.glatalk_project.constant.C
+
+class CountryAdapter {
+    private val countryList:ArrayList<C.NationalCode> = arrayListOf(C.NationalCode.ko, C.NationalCode.en, C.NationalCode.zh, C.NationalCode.ja)
+    private var onCountrySelectListener: OnCountrySelectListener? = null
+
+    fun getItemCount(): Int = countryList.size
+
+    fun setCountrySelectListener(listener: OnCountrySelectListener) {
+        onCountrySelectListener = listener
+    }
+    interface OnCountrySelectListener {
+        fun onCountrySelected(item: C.NationalCode)
+    }
+}
