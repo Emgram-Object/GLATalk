1109ee34665a5a99474f032e850c8c556761cc3a
강민아 <getupmina0405@gmail.com>
2020-11-18 16:06:00 +0900

    값을 뽑아내긴 하는데 setText가 되고 있진 않음 <- 추후 수정 예정

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/MyInfoActivity.kt
@@ -2,6 +2,7 @@ package com.example.glatalk_project.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -13,50 +14,29 @@ import com.example.glatalk_project.R
 import com.example.glatalk_project.TokenData
 import com.example.glatalk_project.network.BaseResponse
 import com.google.gson.JsonElement
+import com.google.gson.internal.LinkedTreeMap
 import kotlinx.android.synthetic.main.activity_my_info.*
 import kotlinx.android.synthetic.main.ui_common_title.*
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.math.log
 
 class MyInfoActivity : AppCompatActivity() {
+
+    var myDao = MyDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_info)
 
-        var myDao = MyDao
+        InfoNetworking()
 
-
-        myDao.detail_info(callback = object : Callback<BaseResponse> {
-            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
-             Log.d("qwert", "onFailure: fail")
-            }
-
-            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
-             Log.d("qwert", "onResponse: success")
-                var result = response.body()!!
-                var resultCode = result.resultCode
-                var desc = result.desc
-                var body = result.body
-
-                Log.d("success", "$body")
-            
+        setTexts()
 
 
 
-                ProfileData.user_name
-
-            }
-//            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
-//                Log.d("qwert", "onFailure: fail")
-//            }
-//            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
-
-////
-//            }
-        })
 
         change_tv.visibility = View.VISIBLE
 
@@ -70,7 +50,51 @@ class MyInfoActivity : AppCompatActivity() {
 
     }
 
+    private fun InfoNetworking(){
+        myDao.detail_info(callback = object : Callback<BaseResponse> {
+            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+                Log.d("qwert", "onFailure: fail")
+            }
 
+            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+                Log.d("qwert", "onResponse: success")
+                var result = response.body()!!
+                var resultCode = result.resultCode
+                var desc = result.desc
+                var body = result.body.toString()
+
+                var jsonObject:JSONObject = JSONObject(body);
+                ProfileData.user_name = jsonObject["user_name"].toString()
+                ProfileData.phone_number = jsonObject["phone_number"].toString()
+                ProfileData.country_cd = jsonObject["country_cd"].toString()
+                ProfileData.user_email= jsonObject["user_email"].toString()
+                ProfileData.user_type = jsonObject["user_type"].toString()
+
+
+                Log.d("result", "$body")
+                Log.d("result", "${ProfileData.user_name}")
+                Log.d("result", "${ProfileData.phone_number}")
+                Log.d("result", "${ProfileData.country_cd}")
+                Log.d("result", "${ProfileData.user_email}")
+                Log.d("result", "${ProfileData.user_type}")
+
+
+            }
+//            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+//                Log.d("qwert", "onFailure: fail")
+//            }
+//            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+
+////
+//            }
+        })
+    }
+    private fun setTexts(){
+        my_info_name_et.setText(ProfileData.user_name)
+        my_info_phone_et.setText(ProfileData.phone_number)
+        my_info_email_et.setText(ProfileData.user_email)
+        my_info_country_select_tv.text = ProfileData.country_cd
+    }
     private fun Pwd_Change() {
         val intent = Intent(this, PwdChangeActivity::class.java)
         startActivity(intent)
@@ -80,12 +104,6 @@ class MyInfoActivity : AppCompatActivity() {
         val intent = Intent(this, InfoChangeActivity::class.java)
         startActivity(intent)
     }
-
-//
-//        my_info_name_et.setText(userVo.user_name)
-//        my_info_phone_et.setText(userVo.phone_number)
-//        my_info_email_et.setText(userVo.user_email)
-//        my_info_country_select_tv.setText(userVo.country_cd)
 
 
     private fun ChangeInfo() {
