5452e7ba395e079d8a43977c8eabd23f7d7831f9
kangmina <getupmina0405@gmail.com>
2020-11-22 15:00:20 +0900

    인텐트 함수 코딩하고 유틸오브젝트 생성

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/FirstActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/FirstActivity.kt
@@ -1,5 +1,6 @@
 package com.example.glatalk_project.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +10,31 @@ class FirstActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //FirstSetLanguage 값이 SharedPreference로 저장, 그 값의 유무를 판단
+
         //if, FirstSetLanguage 값 true -> go to Login Page
             //autoLogin의 유무를 boolean으로 저장 후(sharedPreference), 그 값으로 판
             //if, auto Login == true ,  go to Main
+            //gotoMain
             //else, go to Login   (logout 시 autoLogin 값을 false로 초기화)
-        //else, go to FirstSetLanguage
+            //gotoLogin()
 
+        //else, go to FirstSetLanguage
+        //gotoFirstLang()
     }
+
+    fun gotoMain(){
+        val intentAct = Intent(this,MainActivity::class.java)
+        startActivity(intentAct)
+    }
+
+    fun gotoLogin(){
+        val intentAct = Intent(this,LoginActivity::class.java)
+        startActivity(intentAct)
+    }
+
+    fun gotoFirstLang(){
+        val intentAct = Intent(this,FirstLanguageActivity::class.java)
+        startActivity(intentAct)
+    }
+
 }
---
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/util/SharedPreferenceUtil.kt
@@ -0,0 +1,4 @@
+package com.example.glatalk_project.util
+
+object SharedPreferenceUtil {
+}
