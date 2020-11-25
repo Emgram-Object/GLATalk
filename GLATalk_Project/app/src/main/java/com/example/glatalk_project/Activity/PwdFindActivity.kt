bfc2ef99331d435870cabdb46334a9fe9cd79c79
kangmina <getupmina0405@gmail.com>
2020-11-25 17:49:41 +0900

    비밀번호 찾기 버튼 활성화 완료

--- GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/PwdFindActivity.kt
+++ GLATalk_Project/app/src/main/java/com/example/glatalk_project/Activity/PwdFindActivity.kt
@@ -1,7 +1,10 @@
 package com.example.glatalk_project.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.glatalk_project.Model.UserDAO
 import com.example.glatalk_project.R
@@ -9,6 +12,7 @@ import com.example.glatalk_project.Data.UserData
 import com.example.glatalk_project.network.data.response.BaseResponse
 import com.example.glatalk_project.network.data.request.PwdFindRequest
 import kotlinx.android.synthetic.main.activity_find_pwd.*
+import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,11 +24,11 @@ class PwdFindActivity:AppCompatActivity() {
     var input_user_email:String= " "
     var userData = UserData()
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_pwd)
+
+        var inputId : EditText = find_pwd_id_et
 
         find_pwd_back_btn.setOnClickListener {
             finish_find_pwd()
@@ -34,6 +38,23 @@ class PwdFindActivity:AppCompatActivity() {
             input_user_email = find_pwd_id_et.text.toString()
             send_find_pwd()
         }
+
+        var textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (inputId.text.isNotEmpty()) {
+                    Btn_On()
+                } else {
+                    Btn_Off()
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        }
+
+        inputId.addTextChangedListener(textWatcher)
+
     }
 
 
@@ -63,9 +84,23 @@ class PwdFindActivity:AppCompatActivity() {
         })
     }
 
+    private fun Btn_On() {
+        //색 바꾸기
+        find_pwd_send.isEnabled = true
+        find_pwd_send.setBackgroundResource(R.drawable.rounded_square)
+
+    }
+
+    private fun Btn_Off() {
+        find_pwd_send.isEnabled = false
+        find_pwd_send.setBackgroundResource(R.drawable.rounded_square_dim)
+    }
+
+
     fun finish_find_pwd(){
         val intentAct = Intent(this, LoginActivity::class.java)
         startActivity(intentAct)
         finish()
     }
+
 }
